1b7922d4e78b8559e767374c0d67b82dbf2709b4
Kenneth Wilber <kawgit56@gmail.com>
2024-05-03 17:44:44 -0400

    stuff

--- scripts/prep.py
+++ scripts/prep.py
@@ -1,14 +1,15 @@
-import argparse
-import random
+from argparse import ArgumentParser
+from random import shuffle
 
 from parse import parse_datapoints
 from parse import parse_file
 from parse import save_dataset
 
 
-parser = argparse.ArgumentParser(description='Process the source and destination file paths.')
+parser = ArgumentParser(description='Process the source and destination file paths.')
 parser.add_argument('src_file', type=str, help='The path of the source file. The destination file is assumed to be the src file\'s path minus the file extension plus \'.npz\'')
 args = parser.parse_args()
+
 
 src_file = args.src_file
 dst_file = args.src_file.rsplit(".", 1)[0]
@@ -17,7 +18,7 @@ print("Parsing file...")
 datapoints = parse_file(src_file)
 
 print("Shuffling datapoints...")
-random.shuffle(datapoints)
+shuffle(datapoints)
 
 print("Parsing features...")
 dataset = parse_datapoints(datapoints)
